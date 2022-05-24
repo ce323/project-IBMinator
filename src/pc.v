a7867d3d07a55f9f36b8ec30947987d1298d6c9d
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-05-24 16:15:31 +0430

    nothong

--- src/pc.v
+++ src/pc.v
@@ -1,5 +1,5 @@
-module PC(
-    input clk,rst_b,
+module PC (
+    input clk, rst_b,
     input [31:0] pc_input,
     output reg [31:0] pc_output
 );
@@ -7,7 +7,6 @@ module PC(
 
 always @(posedge clk, negedge rst_b) begin
     if(rst_b == 0)
-        // halted <= 0;
         pc_output <= 1;
     else begin
       pc_output <= pc_input;
