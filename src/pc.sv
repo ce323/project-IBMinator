80ff09f2290ec14f4c92ddff7d1585b68fb40673
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-06-09 04:06:53 +0430

    pc small change

--- src/pc.sv
+++ src/pc.sv
@@ -4,7 +4,7 @@ module pc (
     output reg [31:0] pc_output
 );
 
-always_latch @(posedge clk or negedge rst_b or posedge cache_done) begin
+always_latch @(/*posedge clk or */negedge rst_b or posedge cache_done) begin
     if(rst_b == 0)
         pc_output <= 0;
     else begin
