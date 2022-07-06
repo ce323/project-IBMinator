fea4cbf84d6f2153253c6a92d49aa6fd23c25905
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-07-06 14:32:48 +0430

    some bugs fixed

--- src/EX.sv
+++ src/EX.sv
@@ -1,4 +1,5 @@
-module EX(clk,
+module EX (
+	clk,
 	mem_write_en,
 	mem_to_reg,
 	reg_write,
@@ -13,12 +14,12 @@ module EX(clk,
     rd_num_out,
 	)
 
-input clk , branch , mem_write_en , mem_to_reg , reg_write , zero;
-output[4:0] rd_num;
-input[31:0]alu_result, read_data_2;
-output reg  branch_out , mem_write_en_out , mem_to_reg_out , reg_write_out , zero_out;
+input clk, branch, mem_write_en, mem_to_reg, reg_write, zero;
+output [4:0] rd_num;
+input [31:0]alu_result, read_data_2;
+output reg branch_out, mem_write_en_out, mem_to_reg_out, reg_write_out, zero_out;
 output[4:0] rd_num_out;
-output reg[31:0]alu_result, read_data_2;
+output reg [31:0] alu_result_out, read_data_2_out;
 
 always@(posedge clk){
  mem_write_en_out <= mem_write_en;
